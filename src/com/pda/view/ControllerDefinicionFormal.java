632418b49bb5952adff871ff6048c05c4e0a1a9a
Mateo Puña <mateopuna98@gmail.com>
2018-08-22 12:07:31 -0400

    Funcionalidad de guardado en proceso

--- src/com/pda/view/ControllerDefinicionFormal.java
+++ src/com/pda/view/ControllerDefinicionFormal.java
@@ -1,5 +1,35 @@
 package com.pda.view;
 
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+
 public class ControllerDefinicionFormal {
 
+	
+	@FXML  TextField txt1;
+	
+	@FXML  TextField txt2;
+	
+	@FXML  TextField txt3;
+	@FXML  TextField txt4;
+	@FXML  TextField txt5;
+	@FXML  TextField txt6;
+
+
+
+	@FXML  Button btnComenzar;
+
+	
+	@FXML	
+	public void btnComenzarPresionado(ActionEvent event) throws IOException {
+		
+		
+		System.out.println("Eudaimonia!");
+	}
+	
 }
